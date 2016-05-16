f766198480caceeaff86a757c08011eb13738dfe
Tran Duy Nghiep <tranduynghiep@gmail.com>
2016-05-16 20:54:39 +0700

    comit

---
+++ LearningEnglish/App_Business/Constant/Constants.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace LearningEnglish.Constant
+{
+    public static class Constants
+    {
+        #region "Params"
+        public static int MaxRow = 20;
+        public static int StartIndexRow = 0;
+        #endregion
+
+        #region "Constant"
+        public enum RESULT
+        {
+            SUCCESS = 1,
+            FAILED = 2,
+            UNKNOW_ERROR =3
+        }
+        #endregion
+    }
+
+    public static class MyMutex
+    {
+        #region "Mutex"
+        public static Mutex mutex_add_new_user = new Mutex();
+
+        #endregion
+    }
+}
---
+++ LearningEnglish/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using LearningEnglish.App_Data;
+using LearningEnglish.APP_Service.QuestionAndAnswerService;
+using LearningEnglish.APP_Service.StaffService;
+using LearningEnglish.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LearningEnglish.Constant;
+using LearningEnglish.APP_Service.UserService;
+using System.Threading;
+
+namespace LearningEnglish.Controllers
+{
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        [Route("api/user/create")]
+        public Constants.RESULT create([FromBody] UserModel user)
+        {
+            Constants.RESULT ret = Constants.RESULT.FAILED;
+            if (user != null)
+            {
+                // check exist user name
+                if (!String.IsNullOrEmpty(user.email))
+                {
+                    UserService userSv = new UserService();
+                    // user name not existed
+                    user_tbl userTbl = new user_tbl();
+                    userTbl.first_name = user.firstName;
+                    userTbl.last_name = user.lastName;
+                    userTbl.email1 = user.email;
+                    userTbl.user_name = user.email;
+                    userTbl.password = user.password;
+                    string createdate = DateTime.Now.ToString("yyyyMMdd:HHmmss");
+                    userTbl.create_date = createdate;
+                    userTbl.update_date = createdate;
+                    if (userSv.AddNewUser(userTbl))
+                    {
+                        ret = Constants.RESULT.SUCCESS;
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
