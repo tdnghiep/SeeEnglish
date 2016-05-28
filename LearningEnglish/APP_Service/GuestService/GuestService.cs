3f3d2f8baabe3f3bc98fd9394abed9fc2ae877fc
tmthanh92 <tmthanh92@gmail.com>
2016-05-28 23:03:15 +0700

    add guest (server side)

---
+++ LearningEnglish/APP_Service/GuestService/GuestService.cs
@@ -0,0 +1,35 @@
+﻿using LearningEnglish.App_Data;
+using LearningEnglish.DAL_Service.DAL_GuestService;
+using LearningEnglish.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LearningEnglish.APP_Service.GuestService
+{
+    public class GuestService
+    {
+        DAL_GuestService dalGuestService;
+
+        public GuestService()
+        {
+            dalGuestService = new DAL_GuestService();
+        }
+
+        public Boolean CreateGuestInfo(GuestModel guest)
+        {
+            guest_tbl newGuest = new guest_tbl()
+            {
+                email = guest.email,
+                full_name = guest.fullname,
+                phone_number = guest.phonenumber,
+                notes = guest.notes,
+                sent_files = ""
+            };
+            
+            return dalGuestService.CreateGuestInfo(newGuest);
+        }
+
+    }
+}
